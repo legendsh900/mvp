85ef8e621335281325b5a73214ae22b8a68b81d9
legendsh900 <legendsh20@gmail.com>
2026-01-20 13:31:37 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const PORT = process.env.PORT || 10000;
+
+http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello Render!');
+}).listen(PORT, () => console.log(`Server running on port ${PORT}`));
